b6aac0d28ef80195fee13481153457dd6da26049
Mathias Maes <watcherwhale@maes.family>
2024-08-12 13:39:34 +0200

    openrgb

--- hosts/whaleshark/configuration.nix
+++ hosts/whaleshark/configuration.nix
@@ -6,6 +6,8 @@
 
   networking.hostName = "whaleshark";
 
+  services.hardware.openrgb.enable = true;
+
   # DO NOT TOUCH
   system.stateVersion = "24.05";
 }
--- system/packages.nix
+++ system/packages.nix
@@ -14,6 +14,7 @@
     htop
     unzip
     gcc
+    home-manager
   ];
 
   fonts.packages = with pkgs; [
