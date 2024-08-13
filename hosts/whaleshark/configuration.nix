063cfe9fd7899eb2f5ed9e0e529bef80e93110b0
Mathias Maes <watcherwhale@maes.family>
2024-08-13 21:10:13 +0200

    openrgb?

--- hosts/whaleshark/configuration.nix
+++ hosts/whaleshark/configuration.nix
@@ -1,9 +1,19 @@
-{ ... }:
+{ pkgs, ... }:
 {
   imports = [
     ./hardware-configuration.nix
     ./steam.nix
   ];
+
+  environment.systemPackages = with pkgs; [
+    i2c-tools
+    liquidctl
+  ];
+
+  services.hardware.openrgb = {
+    enable = true;
+    motherboard = "amd";
+  };
 
   networking.hostName = "whaleshark";
 
