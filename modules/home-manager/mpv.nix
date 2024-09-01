72b10e7c66d8ec38069a4b8fccdb5d6cac92356c
Mathias Maes <watcherwhale@maes.family>
2024-09-01 14:19:38 +0200

    mpv

--- modules/home-manager/mpv.nix
+++ modules/home-manager/mpv.nix
@@ -1,46 +1,17 @@
 { pkgs, ... }:
-let
-  show_progress = pkgs.writers.writeLua "show_progress" ''
-    -- from https://github.com/mpv-player/mpv/issues/6592#issuecomment-488269456
-    function on_pause_show_osc(name, value)
-        if value == true then
-            mp.command("no-osd set osd-level 0; script-message osc-visibility always")
-            reset_osd()
-        else
-            mp.command("no-osd set osd-level 0; script-message osc-visibility auto")
-            reset_osd()
-        end
-    end
-
-    -- For some reason `set osd-level 0; script-message osc-visibility always; no-osd set osd-level 1` does not work.
-    -- The OSC messages `OSC visibility: always/auto` are still displayed.
-    -- It's as if `no-osd set osd-level 1` runs before `script-message osc-visibility always` is run/displayed.`¯\_(ツ)_/¯`
-    -- As a workaround, I have to make the osd visible after a 1 second timeout.
-    -- Why? No idea. Bugs?`¯\_(ツ)_/¯`
-    function reset_osd()
-        mp.add_timeout(1,
-            function()
-                mp.command("no-osd set osd-level 1")
-            end
-        )
-    end
-
-    mp.observe_property("pause", "bool", on_pause_show_osc)
-  '';
-in
 {
   programs.mpv = {
     enable = true;
     config = {
       keep-open = true;
       player-operation-mode = "pseudo-gui";
-      save-position-on-quit = true;
+      save-position-on-quit = "";
 
       volume = 80;
 
       ytdl-format = "bestvideo[height<=?1080]+bestaudio/best";
 
-      no-keepaspect-window = true;
+      no-keepaspect-window = "";
     };
 
     bindings = {
@@ -54,9 +25,36 @@ in
 
       s = "playlist-shuffle";
     };
+  };
 
-    scripts = [
-      show_progress
-    ];
+  home.file.".config/mpv/scripts/show_progress.lua" = {
+    text = # lua
+      ''
+        -- from https://github.com/mpv-player/mpv/issues/6592#issuecomment-488269456
+        function on_pause_show_osc(name, value)
+            if value == true then
+                mp.command("no-osd set osd-level 0; script-message osc-visibility always")
+                reset_osd()
+            else
+                mp.command("no-osd set osd-level 0; script-message osc-visibility auto")
+                reset_osd()
+            end
+        end
+
+        -- For some reason `set osd-level 0; script-message osc-visibility always; no-osd set osd-level 1` does not work.
+        -- The OSC messages `OSC visibility: always/auto` are still displayed.
+        -- It's as if `no-osd set osd-level 1` runs before `script-message osc-visibility always` is run/displayed.`¯\_(ツ)_/¯`
+        -- As a workaround, I have to make the osd visible after a 1 second timeout.
+        -- Why? No idea. Bugs?`¯\_(ツ)_/¯`
+        function reset_osd()
+            mp.add_timeout(1,
+                function()
+                    mp.command("no-osd set osd-level 1")
+                end
+            )
+        end
+
+        mp.observe_property("pause", "bool", on_pause_show_osc)
+      '';
   };
 }
