0c5451d404a89d97088929fc53413cf9069c9230
Mathias Maes <watcherwhale@maes.family>
2024-09-01 14:44:53 +0200

    lf

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -11,6 +11,7 @@
     ./devops.nix
 
     ./mpv.nix
+    ./lf
   ];
 
   # automatic garbace collection
---
+++ modules/home-manager/lf/default.nix
@@ -0,0 +1,64 @@
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.lf;
+in
+{
+  options.lf = {
+    enable = lib.mkEnableOption "lf";
+  };
+
+  config = lib.mkIf cfg.enable {
+    xdg.configFile."lf/icons".source = ./icons;
+
+    programs.lf = {
+      enable = true;
+
+      settings = {
+        preview = true;
+        drawbox = true;
+        icons = true;
+        ignorecase = true;
+        hidden = false;
+      };
+
+      commands = {
+        dragon-out = ''%${pkgs.xdragon}/bin/xdragon -a -x "$fx"'';
+        editor-open = ''$$EDITOR $f'';
+        mkdir = ''
+          ''${{
+            printf "Directory Name: "
+            read DIR
+            mkdir $DIR
+          }}
+        '';
+      };
+
+      keybindings = {
+        "<f-7>" = "mkdir";
+        "<backspace>" = "set hidden!";
+        "<backspace2>" = "set hidden!";
+      };
+
+      extraConfig =
+        let
+          previewer = pkgs.writeShellScriptBin "pv.sh" ''
+            file=$1
+            w=$2
+            h=$3
+            x=$4
+            y=$5
+
+            ${pkgs.pistol}/bin/pistol "$file"
+          '';
+        in
+        ''
+          set previewer ${previewer}/bin/pv.sh
+        '';
+    };
+  };
+}
