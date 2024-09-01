a5f6045e0a1645f7273d454888b3f1e4bdceda0b
Mathias Maes <watcherwhale@maes.family>
2024-09-01 13:30:48 +0200

    steam changes

--- modules/programs/steam.nix
+++ modules/programs/steam.nix
@@ -1,9 +1,15 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.steam = {
     enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = false; # Open ports in the firewall for Source Dedicated Server
-    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    remotePlay.openFirewall = false;
+    dedicatedServer.openFirewall = false;
+    localNetworkGameTransfers.openFirewall = true;
   };
+
+  programs.gamemode.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    mangohud
+  ];
 }
