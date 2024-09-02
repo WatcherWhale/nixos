7215a6b392a719d1dd05206200523c98bda9e30f
Mathias Maes <watcherwhale@maes.family>
2024-09-02 08:00:40 +0200

    formatting

--- homes/work/home.nix
+++ homes/work/home.nix
@@ -18,21 +18,21 @@ in
   home.homeDirectory = homePath;
 
   custom.programs.qtile.autostart = pkgs.writeShellScriptBin "autostart" ''
-      ${zellij-store}/bin/zellij-store clean &
-      sh -c 'find ${homePath}/Downloads/* -mtime +50 -type f -exec rm {} \;' &
-      sh -c 'find ~/.config/mpv/watch_later -mtime +30 -print | xargs rm -f' &
-      nm-applet &
-      #/usr/lib/kdeconnectd &
-      #kdeconnect-indicator &
-      #trash-empty 30 &
+    ${zellij-store}/bin/zellij-store clean &
+    sh -c 'find ${homePath}/Downloads/* -mtime +50 -type f -exec rm {} \;' &
+    sh -c 'find ~/.config/mpv/watch_later -mtime +30 -print | xargs rm -f' &
+    nm-applet &
+    #/usr/lib/kdeconnectd &
+    #kdeconnect-indicator &
+    #trash-empty 30 &
 
-      sleep 2
+    sleep 2
 
-      blueman-applet &
-      ${pkgs.zellij}/zellij delete-all-sessions -y &
+    blueman-applet &
+    ${pkgs.zellij}/zellij delete-all-sessions -y &
 
-      # YT-Music
-      gio launch "$HOME/.local/share/applications/youtube-music.desktop" &
+    # YT-Music
+    gio launch "$HOME/.local/share/applications/youtube-music.desktop" &
 
     # Teams
     gio launch "$HOME/.local/share/applications/teams.desktop" &
